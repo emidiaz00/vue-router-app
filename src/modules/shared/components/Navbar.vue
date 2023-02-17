04607ba5bd9bd3fb2c0667170e52967fc1fa0975
emildsn00 <emildsn00@gmail.com>
2023-02-17 17:48:15 -0300

    add router link

---
+++ src/modules/shared/components/Navbar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <router-link to="/">Pokemon List</router-link>
+        <router-link to="/id">PokemonId</router-link>
+        <router-link to="/about">About Page</router-link>
+    </div>
+</template>
+
+<style scoped>
+a {
+    padding: 20px;
+    font-weight: bold;
+    color: #2c3e50;
+    text-decoration: none;
+}
+    
+</style>
