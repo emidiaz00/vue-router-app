755fbfb3a8da121d4fb96e982318ecfac1aa2acc
emildsn00 <emildsn00@gmail.com>
2023-02-21 23:48:58 -0300

    add router link active

--- src/modules/shared/components/Navbar.vue
+++ src/modules/shared/components/Navbar.vue
@@ -2,16 +2,22 @@
     <div>
         <router-link to="/">Pokemon List</router-link>
         <router-link to="/id">PokemonId</router-link>
-        <router-link to="/about">About Page</router-link>
+        <router-link to="/about">About</router-link>
     </div>
 </template>
 
 <style scoped>
-a {
+
+div a {
     padding: 20px;
     font-weight: bold;
     color: #2c3e50;
     text-decoration: none;
 }
+
+.router-link-exact-active {
+  color: #42b983;
+}
+
     
 </style>
