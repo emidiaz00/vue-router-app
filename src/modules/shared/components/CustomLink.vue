eab25f4793265e6a8c20f209ae69eedc080a9949
emildsn00 <emildsn00@gmail.com>
2023-02-25 20:01:58 -0300

    add custom link

---
+++ src/modules/shared/components/CustomLink.vue
@@ -0,0 +1,35 @@
+<template>
+    <a 
+    v-if="isExternalLink"
+    target="_blank"
+    :href="link.to">{{ link.name }}
+</a>
+
+<router-link 
+v-else 
+:to="link.to"
+>
+{{ link.name }}
+</router-link>
+</template>
+
+<script>
+export default {
+    props: {
+        link: {
+            type: Object,
+            require: true
+        }
+    },
+    computed: {
+        isExternalLink() {
+            return this.link.to.startsWith('http')
+        }
+    },
+    
+}
+</script>
+
+<style>
+
+</style>
--- src/modules/shared/components/Navbar.vue
+++ src/modules/shared/components/Navbar.vue
@@ -1,10 +1,38 @@
 <template>
     <div>
-        <router-link :to="{ name: 'home'}">Pokemon List</router-link>
+        <!--<router-link :to="{ name: 'home'}">Pokemon List</router-link>
         <router-link :to="{ name: 'pokemon-id', params: { id:  85}}">PokemonId</router-link>
-        <router-link :to="{ name: 'about'}">About</router-link>
+        <router-link :to="{ name: 'about'}">About</router-link> -->
+        <CustomLink
+            v-for="link in links" 
+            :key="link.to"
+            :link="link"
+        />
+        
     </div>
 </template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+export default {
+    data() {
+        return {
+            links: [
+                { to: '/', name: 'Pokemons'},
+                { to: '/pokemon-id/50', name: 'Por id'},
+                { to: '/about', name: 'About'},
+                { to: 'https://google.com', name: 'Pokemons'}
+            ]
+        }
+    },
+
+    components: {
+        CustomLink: defineAsyncComponent(() => import('./CustomLink.vue'))
+    }
+
+}
+
+</script>
 
 <style scoped>
 
